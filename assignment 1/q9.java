a6fe8df786e53726219c3add774c8419aff7f9b4
Anirudh Sharma <therealanirudhsharma@gmail.com>
2026-09-16 18:47:36 +0530

    add assignment 1/q9.java

---
+++ assignment 1/q9.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class q9 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double n = in.nextDouble();
+        int p = in.nextInt();
+        System.out.println(Math.pow(n, p));
+    }
+}
